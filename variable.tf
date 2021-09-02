5cf681c785735689e4a56fa0a48de4bed03648fc
reginaldobarreto <reginaldo.barreto@gmail.com>
2021-09-01 22:37:24 -0300

    Controle dos nomes de recursos

---
+++ variable.tf
@@ -0,0 +1,6 @@
+#Aqui configuramos todas as nossas preferencias quanto a nome de recursos, bem como localização, etc
+#Para usar dentro do arquivo resources.tf chamamos da seguinte forma "var.nome_da_variavel"
+
+variable "resource_group_name" {
+  default = "RG-Terraform-Lab1"
+}
